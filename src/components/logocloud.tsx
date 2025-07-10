f45d5916c1d38252250ee1a46e2d995e9c5d7a3c
EchheanChhour03 <echhean03@gmail.com>
2025-07-10 22:44:35 +0700

    test

---
+++ src/components/logocloud.tsx
@@ -0,0 +1,77 @@
+import { cn } from "@/lib/utils";
+import { Marquee } from "@/components/magicui/marquee";
+
+const reviews = [
+  {
+    name: "Vercel",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881430/vercel_wordmark_dark_mhv8u8.svg",
+  },
+  {
+    name: "Nextjs",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881475/nextjs_logo_dark_gfkf8m.svg",
+  },
+  {
+    name: "Prime",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg",
+  },
+  {
+    name: "Trustpilot",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tkfspxqmjflfllbuqxsi.svg",
+  },
+  {
+    name: "Webflow",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg",
+  },
+  {
+    name: "Airbnb",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg",
+  },
+  {
+    name: "Tina",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/afqhiygywyphuou6xtxc.svg",
+  },
+  {
+    name: "Stackoverflow",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/ts1j4mkooxqmscgptafa.svg",
+  },
+  {
+    name: "mistral",
+    img: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg",
+  },
+];
+
+const ReviewCard = ({ img }: { img: string }) => {
+  return (
+    <figure
+      className={cn(
+        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}>
+      <img
+        src={img}
+        alt="Logo"
+        className="h-10 w-28 mx-auto brightness-0 dark:invert"
+      />
+    </figure>
+  );
+};
+
+export function MarqueeDemo() {
+  return (
+    <div className="flex justify-center my-4">
+      <div
+        className="relative flex w-[1700px] items-center justify-center group mt-6 gap-6 overflow-hidden p-2"
+        style={{
+          maskImage:
+            "linear-gradient(to left, transparent 0%, black 20%, black 80%, transparent 95%)",
+        }}>
+        <Marquee pauseOnHover className="[--duration:14s]">
+          {reviews.map((review) => (
+            <ReviewCard key={review.name} {...review} />
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        "x-slider": "slide 40s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
